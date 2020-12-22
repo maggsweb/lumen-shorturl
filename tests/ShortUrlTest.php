3d152ddbf8d3c133504e72c81249d0e4331a0a87
Chris <git@maggsweb.co.uk>
2020-12-22 18:55:28 +0000

    Add Unit tests

--- tests/ShortUrlTest.php
+++ tests/ShortUrlTest.php
@@ -1,171 +1,136 @@
 <?php
 
-use App\Models\User;
-
 class ShortUrlTest extends TestCase
 {
 
-    //use \Laravel\Lumen\Testing\Concerns\MakesHttpRequests;
+    public function testMissingHeaderToken()
+    {
+        $this->post('/create');
 
-    /**
-     *
-     */
-//    public function testMissingHeaderToken()
-//    {
-//        $this->post('/create');
-//
-//        $this->seeStatusCode(401);
-//
-//        $this->assertStringContainsString(
-//            'Unauthorized',
-//            $this->response->getContent()
-//        );
-//    }
+        $this->seeStatusCode(401);
 
-    /**
-     *
-     * @TODO change this so an invalid header returns different to a missing header
-     */
-//    public function testInvalidHeaderToken()
-//    {
-//        $header = [
-//            'HTTP_token' => '123456789'
-//        ];
-//
-//        $this->post('/create', [], $header);
-//
-//        $this->seeStatusCode(401);
-//
-//        $this->assertStringContainsString(
-//            'Unauthorized',
-//            $this->response->getContent()
-//        );
-//    }
+        $this->assertStringContainsString(
+            'Unauthorized',
+            $this->response->getContent()
+        );
+    }
 
-    /**
-     * @TODO this isn't actually reading $data
-     */
-//    public function testMissingBody()
-//    {
-//        $data = [
-//           // "long_url"=> "http://www.google.com"
-//        ];
-//        $header = [
-//            'HTTP_token' => $this->user->uuid
-//        ];
-//
-//        $this->post('/create', $data, $header);
-//
-////        dump(
-////            $this->response->getStatusCode(),
-////            $this->response->getContent()
-////        );
-//
-//        $this->seeStatusCode(422); // Validation
-//
-//        $this->assertStringContainsString(
-//            'The long url field is required.',
-//            $this->response->getContent()
-//        );
-//
-////        $this->assertJson('{"long_url":["The long url field is required."]}');
-//    }
+    public function testInvalidHeaderToken()
+    {
+        $header = [
+            'HTTP_token' => '123456789'
+        ];
 
+        $this->post('/create', [], $header);
 
-    /**
-     *
-     */
-//    public function testInvalidBody()
-//    {
-//        $data = [
-//            'long_url' => 'some-invalid-url'
-//        ];
-//
-//        $header = [
-//            'HTTP_token' => $this->user->uuid,
-//            'Content-Type' => 'application/json'
-//        ];
-//
-//        $this->post('/create', $data, $header);
-////        $this->json('POST', '/create', $data, $header);
-////        $this->call('POST', '/create', $data, $header);
-//
-//
-//        dump(
-//            $this->response->getStatusCode(),
-//            $this->response->getContent()
-//
-//        );
-//
-////        $this->seeStatusCode(422); // Validation
-////        $this->assertJson('{"long_url":["The long url field is required."]}');
-//
-//
-//    }
+        $this->seeStatusCode(401);
 
+        $this->assertStringContainsString(
+            'Unauthorized',
+            $this->response->getContent()
+        );
+    }
 
+    public function testMissingBody()
+    {
+        $data = [];
+        $header = [
+            'HTTP_token' => $this->user->uuid
+        ];
 
+        $this->post('/create', $data, $header);
 
+        $this->seeStatusCode(422); // Validation
+        $this->assertJson('{"long_url":["The long url field is required."]}');
+    }
+
+    public function testInvalidBody()
+    {
+        $data = [
+            'long_url' => 'some-invalid-url'
+        ];
+
+        $header = [
+            'HTTP_token' => $this->user->uuid
+        ];
+
+        $this->post('/create', $data, $header);
+
+        $this->seeStatusCode(422); // Validation
+        $this->assertJson('{"long_url":["The long url format is invalid."]}');
+    }
 
     public function testCreateLink()
     {
-        $data = [
-//            'json' => [
-                'long_url' => 'http://www.google.com'
-//            ]
-        ];
-//        dump($data);
-//        dump(json_encode($data));
-//        exit;
+        $url = 'http://www.my-valid-url.com';
 
-//        $string = '{"long_url": "http://www.google.com"}';
-//        $data = json_decode($string, true);
-//        dump($data);
+        $data = [
+            'long_url' => $url
+        ];
 
         $header = [
-//            'HTTP_token' => $this->user->uuid
-            'token' => $this->user->uuid
-            //'Content-Type' => 'application/json'
+            'HTTP_token' => $this->user->uuid
         ];
 
-//        $this->post('/create', $data, $header);
         $this->json('POST', '/create', $data, $header);
 
-        dump(
-            $this->response->getStatusCode(),
+        $this->seeStatusCode(201);
+        $this->assertStringContainsString(
+            json_encode($url),
             $this->response->getContent()
         );
+    }
 
-//        $this->seeStatusCode(200);
+    public function testReturnExistingLink()
+    {
+        $url = 'http://www.my-valid-url.com';
+
+        $data = [
+            'long_url' => $url
+        ];
+
+        $header = [
+            'HTTP_token' => $this->user->uuid
+        ];
+
+        $this->json('POST', '/create', $data, $header);  // 201 New Link
+        $this->json('POST', '/create', $data, $header);  // 200 Existing Link
+
+        $this->seeStatusCode(200);
+        $this->assertStringContainsString(
+            json_encode($url),
+            $this->response->getContent()
+        );
+    }
+
+    public function testUseSuggestedShortCode()
+    {
+        $long_url = 'http://www.my-second-valid-url.com';
+        $short_url = 'shortlink';
+
+        $data = [
+            'long_url' => $long_url,
+            'short_url' => $short_url
+        ];
+
+        $header = [
+            'HTTP_token' => $this->user->uuid
+        ];
+
+        $this->json('POST', '/create', $data, $header);
+
+        $this->seeStatusCode(201);
+        $this->assertStringContainsString(
+            json_encode($long_url),
+            $this->response->getContent()
+        );
+        $this->assertStringContainsString(
+            $short_url,
+            $this->response->getContent()
+        );
     }
 
 
-
-
-//    public function test1()
-//    {
-//        $data = [
-//            'long_url' => 'http://www.google.com'
-//        ];
-//        $content = json_encode($data);
-//
-//        $headers = [
-//            'CONTENT_LENGTH' => mb_strlen($content, '8bit'),
-//            'CONTENT_TYPE' => 'application/json',
-//            'Accept' => 'application/json',
-//            'token' => $this->user->uuid
-//        ];
-//
-//        $this->call(
-//            'POST', '/create', [], [], [], $this->transformHeadersToServerVars($headers), $content
-//        );
-//
-//        dump(
-//            $this->response->getStatusCode(),
-//            $this->response->getContent()
-//        );
-//
-//    }
 
 
 }
